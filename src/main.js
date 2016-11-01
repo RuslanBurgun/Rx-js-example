993fc299dfeabcf7d2bd7ec77a1e1cdcbcd49521
Ruslan Burgun <burgun90@mail.ru>
2016-11-01 14:44:12 +0300

    changes

--- src/main.js
+++ src/main.js
@@ -13,6 +13,41 @@ import $ from 'jquery';
 
 
 //two example
-const data$ = Rx.Observable.interval(3000)
-                .take(5)
-                .subscribe(value=>console.log(value*6));
+// const data$ = Rx.Observable.interval(3000)
+//                 .take(5)
+//                 .subscribe(value=>console.log(value*6));
+
+
+//three example
+// const data = [
+//                 {"title":"one","name":"Ruslan"},
+//                 {"title":"two","name":"Ruslan"},
+//                 {"title":"three","name":"Ruslan"},
+//                 {"title":"four","name":"Ruslan"},
+//             ];
+
+// //filter object and output title
+// const data$ = Rx.Observable.from(data)
+//                 .pluck('title');
+// data$.subscribe(x=>console.log(x));
+
+
+
+//four example (Merge and concat)
+
+// const data$ = Rx.Observable.interval(2000)
+//                 .merge(Rx.Observable.interval(500))
+//                 .take(25)
+//                 .subscribe(x=>console.log(x));
+
+
+
+// const source1 = Rx.Observable.range(1,6).map(v=>'Source_1 '+v);
+// const source2 = Rx.Observable.range(1,10).map(v=>'Source_1 '+v);
+
+// const merge = Rx.Observable.merge(source1, source2)
+//                             .take(7)
+//                             .subscribe(v=>console.log(v));
+
+Rx.Observable.of('hello')
+            .subscribe(x=>Rx.Observable.of(x+" Everyone").subscribe(g=>console.log(g)));
